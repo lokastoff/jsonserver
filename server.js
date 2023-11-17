95ad53f7824944cc4623936b9402ebaf719e547a
Egor Loakstov <mrlokast@gmail.com>
2023-11-17 19:09:31 +0300

    added login

--- server.js
+++ server.js
@@ -8,16 +8,13 @@ const port = 3000;
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 
-// Регистрация нового пользователя
 server.post('/register', (req, res) => {
     const { email, password } = req.body;
   
-    // Проверка наличия email и password в запросе
     if (!email || !password) {
       return res.status(400).json({ error: 'Missing email or password' });
     }
   
-    // Проверка наличия пользователя с таким email в базе данных
     const db = router.db;
     const existingUser = db.get('users').find({ email }).value();
   
@@ -25,7 +22,6 @@ server.post('/register', (req, res) => {
       return res.status(409).json({ error: 'User with this email already exists' });
     }
   
-    // Добавление нового пользователя в базу данных
     db.get('users').push({ email, password }).write();
   
     return res.status(201).json({ message: 'User registered successfully' });
